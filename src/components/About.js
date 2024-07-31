4667c86b7728c1b667007f712f6cb24caf55f282
Jackline Amwenya Nehondo <jnehondo@gmail.com>
2024-07-31 08:52:22 +0300

    updates about element

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div id="bout">
+  return <div id="about">
     <h2>About Me</h2>
     <p>I am a Software Engineer</p>
     <img src={image} alt="I made this"></img>
